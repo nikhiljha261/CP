74377ba5c1c17748014f7ef6b8ffe9f63abe7486
NIKHIL <63747907+nikhiljha261@users.noreply.github.com>
2021-03-26 10:40:20 +0530

    Create starnge_table.cpp

---
+++ starnge_table.cpp
@@ -0,0 +1,43 @@
+//har har mahadev
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define loop(i,a,b)     for(int i=(a);i<(b);i++)
+#define endl            "\n"
+// cout<<fixed<<setprecision(9)<<" ";
+
+void ganeshji(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+
+int main()
+{
+    ganeshji();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    clock_t begin = clock();
+    ll t=1;
+    cin>>t;
+    while(t--)
+    {
+        ll n, m, x;
+        cin >> n >> m >> x;
+        x--;
+        ll col = x / n;
+        ll row = x % n;
+        cout << row * m + col + 1 << "\n";
+
+    }
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif
+}
+
