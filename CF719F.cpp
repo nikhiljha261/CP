4bd260d96d8961fc52a9b5ed7d59cc39210cb7e1
NIKHIL <63747907+nikhiljha261@users.noreply.github.com>
2021-05-06 12:51:55 +0530

    Create CF719F.cpp

---
+++ CF719F.cpp
@@ -0,0 +1,62 @@
+//har har mahadev
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define loop(i,a,b)     for(int i=(a);i<(b);i++)
+#define endl            "\n"
+// cout<<fixed<<setprecision(9)<<" ";
+// double rounded = std::floor((V * 100) + .5) / 100;
+
+void ganeshji(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+
+int main()
+{
+    ganeshji();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    clock_t begin = clock();
+    ll t=1;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        if(n<=9)
+        {
+            cout<<n<<endl;
+            continue;
+        }
+        ll ans=0;
+        loop(i,1,10)
+        {
+            ll no=0;
+            loop(j,0,10)
+            {
+                no=no*10+i;
+                if(no<=n)
+                {
+                    ans++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        cout<<ans<<endl;
+
+    }
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif
+}  
